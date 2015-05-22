c004e34a4082ebb69d2a4f782d43d741e84f399c
maraki87 <maria_luppi@libero.it>
2015-05-22 16:46:48 +0200

    final version

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,8 @@
 # ASSIGNMENT 2
 
-# 
+# I need to create a function that set the value of a matrix, get the value of that matrix, 
+# set the value of the inverse matrix and get the value of the inverse matrix. To do that I write:
+
 makeCacheMatrix = function(x = matrix()) {
 m = NULL
 set = function(y) {
@@ -14,7 +16,10 @@ getinverse = function() m
 list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
   
-#
+# Now I want to create a function that checks if the inverse of a matrix has already been calculated. 
+# If so, it gets this inverse from the cache and skips the computation. 
+# Otherwise, it computes the inverse of the data and sets the value of the inverse matrix in the cache via the setmean function.
+# To do that I write:
 
 cacheSolve = function(x) {
 m = x$getinverse()
@@ -28,8 +33,8 @@ m = solve(data)
 x$setinverse(m)
 m
 }
-  
-# Now I want to try if the fucntion works; so I create a matrix and compute its inverse, by recalling 
+
+# Now I want to try if the function works; so I create a matrix and compute its inverse, by recalling 
 # the function created above
 
 testMatrix = rbind(c(1,2,3), c(0,0,1), c(1,0,0))
